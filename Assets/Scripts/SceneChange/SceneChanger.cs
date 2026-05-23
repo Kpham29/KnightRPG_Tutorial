30b1a717218a8947639920f10d1920a6a8a9a536
kiet <phamkiet29xt@gmail.com>
2026-05-23 22:21:54 +0700

    complete change scene

---
+++ Assets/Scripts/SceneChange/SceneChanger.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public string sceneToLoad;
+    public Animator fadeAnim;
+    public float fadeTime = .5f;
+    public Vector2 newPlayerPosition;
+    private Transform player;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            player = other.transform;
+            fadeAnim.Play("FadeToWhite");
+            StartCoroutine(DelayFade());
+        }
+    }
+
+    IEnumerator DelayFade()
+    {
+        yield return new WaitForSeconds(fadeTime);
+        player.position = newPlayerPosition;
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
